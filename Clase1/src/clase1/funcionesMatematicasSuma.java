73cdee1ee2f5ecba463da9f63205dd19b04700d8
Marlon22222 <Marloninteriano64@gmail.com>
2020-08-14 23:43:35 -0600

    PRUEBA 1
    FUNCIONES + COCHE

---
+++ Clase1/src/clase1/funcionesMatematicasSuma.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1;
+
+/**
+ *
+ * @author ANGELK101217
+ */
+public class funcionesMatematicasSuma {
+    
+//        System.out.println(Resultado);
+        
+        int Suma (){
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado;
+        Resultado = dato1+dato2;
+        
+        return Resultado;
+        
+        }
+        
+        int Resta (){
+        
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado;
+        Resultado = dato1-dato2;
+        
+        return Resultado;
+        
+        
+        }
+        
+        double division (){
+            
+            
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado;
+        Resultado = dato1/dato2;
+        
+        return Resultado;
+        }
+}
